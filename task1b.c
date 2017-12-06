d80249b9f5f30738693d5a79597a730ed9285b3a
mcgime01 <mcgime01@luther.edu>
2017-12-06 22:51:20 +0000

    Update task1b.c

--- task1b.c
+++ task1b.c
@@ -1,15 +1,22 @@
-// make queue (linked list)
-//gereate processes and put them in the linked list as they are gereated
+
 
 //implement the RR algorithm
 // A fixed time is allotted to every process that arrives in the queue. This fixed time is known as time slice or time quantum.
-	// The first process that arrives is selected and sent to the processor for execution.If it is not able to complete its execution within the time quantum provided, then an interrupt is generated using an automated timer.
-	//The process is then stopped and is sent back at the end of the queue.However, the state is saved and context is thereby stored in memory.This helps the process to resume from the point where it was interrupted.
-	//The scheduler selects another process from the ready queue and dispatches it to the processor for its execution.It is executed until the time Quantum does not exceed.
-	//The same steps are repeated until all the process are finished.
-//calculate avrage response time and and turnaround time 
-//print to output to file
 
+	// The first process that arrives is selected and sent to the processor for execution.
+// If it is not able to complete its execution within the time quantum provided, then an interrupt is generated using an automated timer.
+
+	//The process is then stopped and is sent back at the end of the queue.
+// However, the state is saved and context is thereby stored in memory.
+// This helps the process to resume from the point where it was interrupted.
+
+	//The scheduler selects another process from the ready queue and dispatches it to the processor for its execution.
+// It is executed until the time Quantum does not exceed.
+
+//The same steps are repeated until all the process are finished.
+
+//calculate average response time and and turnaround time
+//print to output to file
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -38,12 +45,20 @@ int main() {
 
 
    while (head != NULL) {
-        simulateRoundRobinProcess(myProcessPtr, StartTime, EndTime);
-        head = tail;
-        head->next = head;
-    }
+       printf("about to run round robin");
+       simulateRoundRobinProcess(myProcessPtr, StartTime, EndTime);  	// The first process that arrives is selected and sent to the processor for execution.
+       printf(" ran Round Robin");
 
-    //RoundRobin
+       if (head->data->iBurstTime == 0) {
+           current = tail;         //The process is then stopped and is sent back at the end of the queue.
+           current->next = head;
+           current->next = current;  //can I do this?
+       } else {
+           deleteNode(head, current);
+       }
+   }
 
+    //The scheduler selects another process from the ready queue and dispatches it to the processor for its execution.
+    
 
-
+}

a6399ac4bf25f175eabf52ce64b72d341a9f2f9e
mcgime01 <mcgime01@luther.edu>
2017-12-06 00:58:02 +0000

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -5,20 +5,23 @@ struct node {
     struct process * data;
     int key;
     struct node * next;
+    struct node * previous;
 };
 
-struct node *head;
-struct node *current;
-struct node *tail;
+//struct node *head;
+//struct node *current;
+//struct node *tail;
 
 //display the list
 void printList();
+//print processes data for debug
+void printProcess(struct process *process);
 //creates and inserts new node in list by order of burstTime
-void insertByBurstTime(int key, struct node *current_node, struct process *process_to_insert, struct node *tail);
+void insertByBurstTime(int key, struct node **current_node, struct process *process_to_insert, struct node **tail);
 //insert link at the first location
 void insertFirst(int key, struct process *data);
 //insert link at end of list
-void insertLast(int key, struct process *data);
+void insertLast(int key, struct node **current_node, struct process *process_to_insert);
 //delete first item
 struct node* deleteFirst();
 // length of List

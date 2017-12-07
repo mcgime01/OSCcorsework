da5986c1f63fdcc33eb2e0029d120130ee7ff6d2
mcgime01 <mcgime01@luther.edu>
2017-12-07 21:16:25 +0000

    Update task1a.c

--- task1a.c
+++ task1a.c
@@ -4,42 +4,55 @@
 #include "linkedlist.h"
 
 int main() {
-  // defining Variables
+  //defining Variables
   struct node *head = NULL;
   struct node *current = NULL;
   struct node *tail = NULL;
   struct timeval *StartTime = (struct timeval *)malloc(sizeof(struct timeval));
   struct timeval *EndTime = (struct timeval *)malloc(sizeof(struct timeval));
   struct process *myProcessPtr;
+  int n, sumRT, sumTAT;
+  double num[10];
+  float averageRTinsec, averageTATinsec;
 
-  // Creating 10 Processes by calling generateProcess
+  FILE *f = fopen("task1a.txt", "w");
+  if (f == NULL) {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+
+  //creating 10 Processes by calling generateProcess
   for (int i = 0; i < 10; i++) {
     myProcessPtr = generateProcess();
     head = insertByBurstTime(head, myProcessPtr, i);
   }
-  printList(head);
 
+// Running Processes through CPU
   struct node *currentProcessPtr = NULL;
   currentProcessPtr = head;
 
-  while (currentProcessPtr != NULL) {  // stop if list is empty
-
-    // while (myProcessPtr->iState == 1) {  //skip node if already run and
-    // istate changed
-    printf("Getting ready to run SJF for process: %d\n",
-           currentProcessPtr->data->iProcessId);
+  while (currentProcessPtr != NULL) { // stop if list is empty
+    int BurstTime = currentProcessPtr->data->iBurstTime;
     simulateSJFProcess(currentProcessPtr->data, StartTime, EndTime);
     if (currentProcessPtr->data->iState == FINISHED) {
-      printf(
-          " -- Process: %d (index %d) finished. Creation time: %d, StartTime: "
-          "%d, EndTime, %d\n",
-          currentProcessPtr->data->iProcessId, currentProcessPtr->key,
-          currentProcessPtr->data->oTimeCreated, *StartTime, *EndTime);
+        //Calculate ResponseTime and TurnAroundTime
+        int ResponseTime = getDifferenceInMilliSeconds(currentProcessPtr->data->oTimeCreated, *StartTime);
+        int TurnAroundTime = getDifferenceInMilliSeconds(currentProcessPtr->data->oTimeCreated, *EndTime);
 
+        //print data to file
+        fprintf(f, "Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d \n",
+        currentProcessPtr->data->iProcessId, BurstTime, currentProcessPtr->data->iBurstTime, ResponseTime, TurnAroundTime);
+
+        //to calculate Averages
+        n++;
+        sumRT += num[n] = ResponseTime;
+        sumTAT += num[n] = TurnAroundTime;
+
+        // and get rid of run processes
       if (currentProcessPtr->next != NULL) {
         currentProcessPtr = currentProcessPtr->next;
         deleteNode(&head, currentProcessPtr->previous);
-      } else {
+      } else{
         struct node *tmp = currentProcessPtr;
         currentProcessPtr = currentProcessPtr->next;
         deleteNode(&head, tmp);
@@ -47,26 +60,20 @@ int main() {
     }
   }
 
-  // and write output to file
+    int averageRT = sumRT/n;
+    averageRTinsec = averageRT/1000;
+    printf("%d \n",n);
+    printf("%d \n", averageRT);
+    fprintf(f, "Average Response Time =\"%.6f\" \n", averageRTinsec);
 
-  /*//write to file
-  FILE *f = fopen("task1a.txt", "w");
-  if (f == NULL) {
-          printf("Error opening file!\n");
-          exit(1);
-  }
-  // print some text
-  const char *text = "Write this to the file";
-  fprintf(f, "Some text: %s\n", text);
+    int averageTAT = sumTAT / n;
+    averageTATinsec = averageTAT/1000;
+    printf("%d \n",n);
+    printf("%d \n", averageTAT);
 
-  // print integers and floats
-  int m = 1;
-  float py = 3.1415927;
-  fprintf(f, "Integer: %d, float: %f\n", i, py);
 
-  // printing single characters
-  char c = 'A';
-  fprintf(f, "A character: %c\n", c);
+    fprintf(f, "Average Turn Around Time =\"%.6f\" \n", averageTATinsec);
 
-  fclose(f);*/
+  fclose(f);
 }
+
